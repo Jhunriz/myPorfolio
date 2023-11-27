322ddc421e8a6eb9788bf06f1cc7abd951749b79
Jhunriz <lalata.jhunriz.bscs2019@gmail.com>
2023-11-27 15:35:37 +0800

    update_porfolio

--- resources/js/Layouts/SideNav.jsx
+++ resources/js/Layouts/SideNav.jsx
@@ -1,18 +1,36 @@
 import { Link } from "@inertiajs/react";
-import React from "react";
+import { React, useState } from "react";
 
 export default function SideNav() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
-        <div className="flex flex-grow gap-5 justify-center items-center p-9 shadow-2xl position-fixed">
-            <p className="hover:font-bold cursor-pointer">Home</p>
-            <p className="hover:font-bold cursor-pointer">Services</p>
-            <p className="hover:font-bold cursor-pointer">About</p>
-            <Link href="skills">
-                <p className="hover:font-bold cursor-pointer">Skills</p>
-            </Link>
-            <p className="hover:font-bold cursor-pointer">Porfolio</p>
-            <p className="hover:font-bold cursor-pointer">Blog</p>
-            <p className="hover:font-bold cursor-pointer">Contacts</p>
+        <div className="sticky lg:flex- lg:flex-grow gap-5 justify-center items-center p-10 lg:p-8 shadow-2xl w-full top-0 bg-white z-50">
+            <div
+                className="lg:hidden justify-end cursor-pointer"
+                onClick={toggleMenu}
+            >
+                <ion-icon name="menu-outline"></ion-icon>
+            </div>
+            <div
+                className={`lg:flex lg:justify-center flex flex-col lg:flex-row items-center gap-5 ${
+                    menuOpen ? "flex" : "hidden"
+                } mt-5`}
+            >
+                <p className="hover:font-bold cursor-pointer ">Home</p>
+                <p className="hover:font-bold cursor-pointer">Services</p>
+                <p className="hover:font-bold cursor-pointer">About</p>
+                <Link href="skills">
+                    <p className="hover:font-bold cursor-pointer">Skills</p>
+                </Link>
+                <p className="hover:font-bold cursor-pointer">Portfolio</p>
+                <p className="hover:font-bold cursor-pointer">Blog</p>
+                <p className="hover:font-bold cursor-pointer">Contacts</p>
+            </div>
         </div>
     );
 }
